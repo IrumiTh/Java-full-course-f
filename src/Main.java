f6eecc7701565a1aa9fa58b8d0ac4bfb460a6815
irumi <irumiabeywickrama@gmail.com>
2024-06-05 18:59:14 +0530

    Random class

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
-		double x ;
-		double y ;
-		double z;
-		
-		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		int x = random.nextInt();
 		
 		
-		System.out.println("Enter side x:");
-		x = scanner.nextDouble();
 		
-		System.out.println("Enter side y:");
-		y = scanner.nextDouble();
+		/*for getting a number between 0 to 6 
+			
+			*int x = random.nextInt(6);
+		*/
 		
-		z = Math.sqrt((x*x) + (y*y));
-
-		System.out.println("The hypotenuse is:"+z);
+		double y = random.nextDouble();
 		
-		scanner.close();
+		
+		boolean z = random.nextBoolean();
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 		
 		
 	}
