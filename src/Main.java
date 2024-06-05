2eb1d07d6299224c3664b04fe8a38ef1a4e9d2f3
irumi <irumiabeywickrama@gmail.com>
2024-06-05 15:09:44 +0530

    Math class part(II)

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,27 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		double x = 3.14;
-		double y = -10;
-		int q = 10;
+		double x ;
+		double y ;
+		double z;
 		
-		double z = Math.min(x, y);
-		System.out.println(z);
+		Scanner scanner = new Scanner(System.in);
 		
-		z = Math.max(x, y);
-		System.out.println(z);
 		
-		z = Math.abs(y);
-		System.out.println(z);
+		System.out.println("Enter side x:");
+		x = scanner.nextDouble();
 		
-		z = Math.sqrt(q);
-		System.out.println(z);
+		System.out.println("Enter side y:");
+		y = scanner.nextDouble();
 		
-		z = Math.round(x);
-		System.out.println(z);
+		z = Math.sqrt((x*x) + (y*y));
+
+		System.out.println("The hypotenuse is:"+z);
 		
-		z = Math.ceil(x);
-		System.out.println(z);
+		scanner.close();
 		
-		z = Math.floor(x);
-		System.out.println(z);
+		
 	}
 
 }
