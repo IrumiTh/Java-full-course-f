319e67cb0fb267771f3dcc7e62693744dacb1a34
irumi <irumiabeywickrama@gmail.com>
2024-06-05 23:09:35 +0530

    while loop

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
-import java.util.Random;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		String day = "Fr";
-		switch(day) {
-		case "Sunday": System.out.println("It is Sunday");
-		break;
-		case "Monday": System.out.println("It is MOnday");
-		break;
-		case "Tuesday": System.out.println("It is tuessday");
-		break;
-		case "Wednesday": System.out.println("It is wednesday");
-		break;
-		case "Thursday": System.out.println("It is thursday");
-		break;
-		case "Friday": System.out.println("It is friday");
-		break;
-		case "Saturday": System.out.println("It is Sadurday");
-		break;
-		default: System.out.println("It is not a day");
-		
-		
-		}
+			
+			Scanner scanner =  new Scanner(System.in);
+			String name = "";
+			
+			while(name.isBlank()) {
+				System.out.print("Enter your name: ");
+				name = scanner.nextLine();
+			}
+			System.out.print("Hello " + name);
 		
 	}
 
