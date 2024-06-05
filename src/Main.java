be715521e9c8d419e0c21d977716278497130963
irumi <irumiabeywickrama@gmail.com>
2024-06-05 22:54:25 +0530

    switches

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,27 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
+		String day = "Fr";
+		switch(day) {
+		case "Sunday": System.out.println("It is Sunday");
+		break;
+		case "Monday": System.out.println("It is MOnday");
+		break;
+		case "Tuesday": System.out.println("It is tuessday");
+		break;
+		case "Wednesday": System.out.println("It is wednesday");
+		break;
+		case "Thursday": System.out.println("It is thursday");
+		break;
+		case "Friday": System.out.println("It is friday");
+		break;
+		case "Saturday": System.out.println("It is Sadurday");
+		break;
+		default: System.out.println("It is not a day");
 		
-		int age = 14;
-		if(age>=18) {
-			System.out.println("You are an adult");
+		
 		}
-		else if(age>=13){
-			System.out.println("You are a teenager");
-		}
-		else {
-			System.out.println("you are not an adult");
-		}
+		
 	}
 
 }
