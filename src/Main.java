69e79d1b8b0c4f122b60bb45a0c8c148c4cca6cb
irumi <irumiabeywickrama@gmail.com>
2024-06-05 19:28:19 +0530

    if statements

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,17 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-		int x = random.nextInt();
 		
-		
-		
-		/*for getting a number between 0 to 6 
-			
-			*int x = random.nextInt(6);
-		*/
-		
-		double y = random.nextDouble();
-		
-		
-		boolean z = random.nextBoolean();
-		
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		
+		int age = 14;
+		if(age>=18) {
+			System.out.println("You are an adult");
+		}
+		else if(age>=13){
+			System.out.println("You are a teenager");
+		}
+		else {
+			System.out.println("you are not an adult");
+		}
 	}
 
 }
