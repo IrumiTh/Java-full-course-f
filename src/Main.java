1fb6a4f23a4b01f2bf9e5c9d98fb33fbdb79af02
irumi <irumiabeywickrama@gmail.com>
2024-06-05 14:48:36 +0530

    GUI intro

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Enter your name:");
+		JOptionPane.showMessageDialog(null, "Hello," +name);
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("What is your name:");
-		String name = scanner.nextLine();
-		System.out.println("How old are you:");
-		int age = scanner.nextInt();
-		scanner.nextLine();
-		System.out.println("What is your favourite food:");
-		String food = scanner.nextLine();
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age:"));
+		JOptionPane.showMessageDialog(null, "You are " +age+" years old");
 
-		
-		
-		System.out.println("Hello,"+name);
-		System.out.println("You are "+age + " iruyears old");
-		System.out.println("You like "+food);
-
-		
-
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height:"));
+		JOptionPane.showMessageDialog(null, "You are " +height+" cm tall");
 	}
 
 }
