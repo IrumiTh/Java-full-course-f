95c109a1d8b6ce840c762c531c89db1621b174f5
irumi <irumiabeywickrama@gmail.com>
2024-06-08 14:07:15 +0530

    2D ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> Numbers = new ArrayList<Integer>();
-		Numbers.add(23);
-		Numbers.add(40);
-		Numbers.add(57);
-		
-		Numbers.set(0, 80);
-		Numbers.remove(0);
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
 		
 		
-		for(int i=0; i< Numbers.size();i++) {
-			System.out.println(Numbers.get(i));
-		}
+		ArrayList<String> bakeryList= new ArrayList();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList= new ArrayList();
+		produceList.add("tomato");
+		produceList.add("zuchini");
+		produceList.add("peppers");
+		
+		
+		ArrayList<String> drinkList= new ArrayList();
+		drinkList.add("soda");
+		drinkList.add("tea");
+		
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinkList);
+		
+		System.out.print(groceryList);
 		
 	}
 
