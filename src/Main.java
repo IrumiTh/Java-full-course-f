95d41adfc2c24a339b5036555ab3b81400268cca
irumi <irumiabeywickrama@gmail.com>
2024-06-08 10:09:56 +0530

    2D

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-			//String[] car = {"CAMARO","CORVETTE","TESLA"};
-			//car[0] = "MUSTANG";
-			//System.out.println(car[0]);
-		String[] cars = new String[3];
-		cars[0] = "camaro";
-		cars[1] = "corvette";
-		cars[2] = "tesla";
-		for(int i=0;i<3;i++) {
-			System.out.println(cars[i]);
+		Scanner scanner = new Scanner(System.in);
+		
+		String[][]  names = new String[2][2];
+		for (int i=0 ; i<2 ; i++ ) {
+			for(int j=0 ; j<2 ; j++ ) {
+				System.out.print("Enter a name: ");
+				names[i][j]= scanner.next();
+				
+			}
 		}
+		
+		for (int i=0 ; i<names.length ; i++ ) {
+			System.out.println();
+			for(int j=0 ; j<names[i].length ; j++ ) {
+				System.out.print(names[i][j] + " ");
+				
+			}
+		}
+		
 		
 	}
 
