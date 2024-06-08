d961514140b2be4c6432fab6dfe26008ab554f93
irumi <irumiabeywickrama@gmail.com>
2024-06-08 15:42:32 +0530

    overloade methods

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String name = "IRUMI";
-		int age = 23;
-		hello(name, age);
+		System.out.println(sum(3,4));
+		System.out.println(sum(3,4,5));
+		System.out.println(sum(3,4,5,6));
 		
 		
-		int x=3;
-		int y=4;
-		System.out.println(sum(x,y));
-		
-		
-	}
-	static void hello(String name, int a) {
-		System.out.println("Hello, "+name);
-		System.out.println("you are "+a);
 	}
 	
 	static int sum(int x, int y) {
+		System.out.println("This is overloaded method 1");
 		return (x+y);
+	}
+	
+	static int sum(int x, int y, int z) {
+		System.out.println("This is overloaded method 2");
+		return (x+y+z);
+	}
+	
+	static int sum(int x, int y, int z, int w) {
+		System.out.println("This is overloaded method 3");
+		return (x+y+z+w);
 	}
 
 }
