d3d42a2b64ab2b2924960879aee076137b5d656b
irumi <irumiabeywickrama@gmail.com>
2024-06-09 08:48:43 +0530

    constructors

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Car mycar = new Car();
-		System.out.println(mycar.model);
-		System.out.println(mycar.make);
-		System.out.println(mycar.year);
-		mycar.drive();
-		mycar.brake();
-		
-		
-		Car mycar2 = new Car();
-		mycar2.brake();
-		System.out.println(mycar2.price);
-		
+		Human human = new Human("Risk", 23, 70);
+		Human human2= new Human("John",27,73);
+
+		System.out.println(	human.name);
+		System.out.println(human2.name);
 		
 	}
 	
